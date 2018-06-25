ce9c071418430756ea77539a40341f6a0f3857b4
nighttidesy <378836940@qq.com>
2018-06-25 09:52:16 +0800

    add start.py

---
+++ bin/start.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author heaven
+
+import sys,os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(BASE_DIR)
+print(sys.path)
